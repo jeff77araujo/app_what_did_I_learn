921ce093c5bd56cce98c0a48df63dd1b58cd5736
Jeff Araujo <jefferson_lord@yahoo.com.br>
2021-01-20 18:45:26 -0300

    Implementado os métodos viewHolder

--- app/src/main/java/com/app/whatdidilearn/view/LearnedItemsAdapter.kt
+++ app/src/main/java/com/app/whatdidilearn/view/LearnedItemsAdapter.kt
@@ -2,14 +2,26 @@ package com.app.whatdidilearn.view
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import com.app.whatdidilearn.R
 import com.app.whatdidilearn.entities.ItemLearned
 
 class LearnedItemsAdapter: RecyclerView.Adapter<LearnedItemsAdapter.LearnedItemViewHolder>() {
     var data = listOf<ItemLearned>()
     inner class LearnedItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val titleItem: TextView = itemView.findViewById(R.id.itemTitle)
+        val descriptionItem: TextView = itemView.findViewById(R.id.itemDescription)
+        val levelItem: View = itemView.findViewById(R.id.itemLevel)
 
+        fun bind(title: String, description: String, color: Int) {
+            titleItem.text = title
+            descriptionItem.text = description
+            levelItem.setBackgroundResource(color)
+        }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearnedItemViewHolder {
         TODO("Not yet implemented")
