553f24b3bcca5629f25a2a88e5eec625a205c2bc
Jeff Araujo <jefferson_lord@yahoo.com.br>
2021-01-20 14:47:15 -0300

    implementação do Recycle View

---
+++ app/src/main/java/com/app/whatdidilearn/data/DatabaseItems.kt
@@ -0,0 +1,38 @@
+package com.app.whatdidilearn.data
+
+import com.app.whatdidilearn.entities.ItemLearned
+import com.app.whatdidilearn.entities.Level
+
+class DatabaseItems {
+    fun getAll(): List<ItemLearned> {
+        val itemOne = ItemLearned(
+                "Kotlin - Null safety",
+                "O sistema de tipos de Kotlin visa eliminar o perigo de referências nulas do código",
+                understandingLevel = Level.HIGH
+        )
+
+        val itemTwo = ItemLearned(
+                "Layout editor",
+                "O Design Editor exibe o layout em vários dispositivos e versões do Android." +
+                            "É possível criar e editar um layout usando apenas componentes visuais",
+                understandingLevel = Level.MEDIUM
+        )
+
+        val itemThree = ItemLearned(
+                "Git",
+                "É um sistema de controle de versão distribuido. Com ele é possível rastrear " +
+                            "mudanças no código fonte durante o desenvolvimento de software's",
+                understandingLevel = Level.LOW
+        )
+
+        val itemFour = ItemLearned(
+                "GroupView",
+                "É uma view especial que pode conter outras views (chamadas de filhos). " +
+                            "É a classe base para layouts e containers de views",
+                understandingLevel = Level.MEDIUM
+        )
+
+        return listOf(itemOne, itemTwo, itemThree, itemFour)
+
+    }
+}
--- app/src/main/java/com/app/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/app/whatdidilearn/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.app.whatdidilearn
+package com.app.whatdidilearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.whatdidilearn.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
