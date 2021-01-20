9bc8f2056b845d1af634a4c7b67648ad8af50f3f
Jeff Araujo <jefferson_lord@yahoo.com.br>
2021-01-20 11:37:24 -0300

    criado Level and ItemLearned

---
+++ app/src/main/java/com/app/whatdidilearn/entities/ItemLearned.kt
@@ -0,0 +1,7 @@
+package com.app.whatdidilearn.entities
+
+data class ItemLearned(
+    val title: String,
+    val description: String,
+    val understandingLevel: Level
+)
---
+++ app/src/main/java/com/app/whatdidilearn/entities/Level.kt
@@ -0,0 +1,5 @@
+package com.app.whatdidilearn.entities
+
+enum class Level {
+    HIGH, MEDIUM, LOW
+}
