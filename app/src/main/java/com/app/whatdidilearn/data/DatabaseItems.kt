98221582e801ef5738f780934fd91e7530806f20
Jeff Araujo <jefferson_lord@yahoo.com.br>
2021-01-22 13:46:28 -0300

    criado room das Entities e implementação de dependências e plugins no build.gradle do app e do projeto

--- app/src/main/java/com/app/whatdidilearn/data/DatabaseItems.kt
+++ app/src/main/java/com/app/whatdidilearn/data/DatabaseItems.kt
@@ -1,41 +1,41 @@
 package com.app.whatdidilearn.data
 
 import com.app.whatdidilearn.entities.ItemLearned
-import com.app.whatdidilearn.entities.Level
+import com.app.whatdidilearn.entities.UnderstandingLevel
 
 class DatabaseItems {
     fun getAll(): List<ItemLearned> {
         val itemOne = ItemLearned(
                 "Kotlin - Null safety",
                 "O sistema de tipos de Kotlin visa eliminar o perigo de referências nulas do código",
-                understandingLevel = Level.HIGH
+                understandingLevel = UnderstandingLevel.HIGH
         )
 
         val itemTwo = ItemLearned(
                 "Layout editor",
                 "O Design Editor exibe o layout em vários dispositivos e versões do Android." +
                             "É possível criar e editar um layout usando apenas componentes visuais",
-                understandingLevel = Level.MEDIUM
+                understandingLevel = UnderstandingLevel.MEDIUM
         )
 
         val itemThree = ItemLearned(
                 "Git",
                 "É um sistema de controle de versão distribuido. Com ele é possível rastrear " +
                             "mudanças no código fonte durante o desenvolvimento de software's",
-                understandingLevel = Level.LOW
+                understandingLevel = UnderstandingLevel.LOW
         )
 
         val itemFour = ItemLearned(
                 "GroupView",
                 "É uma view especial que pode conter outras views (chamadas de filhos). " +
                             "É a classe base para layouts e containers de views",
-                understandingLevel = Level.MEDIUM
+                understandingLevel = UnderstandingLevel.MEDIUM
         )
 
         val itemFive = ItemLearned(
                 "View Binding",
                 "View Binding é um recurso que facilita a programação de códigos que interagem com views ",
-                understandingLevel = Level.MEDIUM
+                understandingLevel = UnderstandingLevel.MEDIUM
         )
 
         return listOf(itemOne, itemTwo, itemThree, itemFour, itemFive)
--- app/src/main/java/com/app/whatdidilearn/entities/UnderstandingLevel.kt
+++ app/src/main/java/com/app/whatdidilearn/entities/UnderstandingLevel.kt
@@ -2,7 +2,7 @@ package com.app.whatdidilearn.entities
 
 import com.app.whatdidilearn.R
 
-enum class Level (val color:  Int) {
+enum class UnderstandingLevel (val color:  Int) {
     HIGH(R.color.purple_200),
     MEDIUM(R.color.purple_500),
     LOW(R.color.purple_700)
